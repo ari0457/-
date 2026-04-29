1f8f58fa473681e1beaea7d517f75a0e1462e2a4
ari0457 <a04571069@gmail.com>
2026-04-29 09:33:38 +0300

    Добавлены новые задания

---
+++ Program11.2.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+interface ICar
+{
+    string GetFeatures();
+    double GetPrice();
+}
+
+class BasicCar : ICar
+{
+    public string GetFeatures()
+    {
+        return "Базовая комплектация: двигатель, колёса, руль";
+    }
+
+    public double GetPrice()
+    {
+        return 15000;
+    }
+}
+
+abstract class CarDecorator : ICar
+{
+    protected ICar car;
+
+    public CarDecorator(ICar car)
+    {
+        this.car = car;
+    }
+
+    public virtual string GetFeatures()
+    {
+        return car.GetFeatures();
+    }
+
+    public virtual double GetPrice()
+    {
+        return car.GetPrice();
+    }
+}
+
+class SunroofDecorator : CarDecorator
+{
+    public SunroofDecorator(ICar car) : base(car) { }
+
+    public override string GetFeatures()
+    {
+        return car.GetFeatures() + ", люк на крыше";
+    }
+
+    public override double GetPrice()
+    {
+        return car.GetPrice() + 1000;
+    }
+}
+
+class NavigationDecorator : CarDecorator
+{
+    public NavigationDecorator(ICar car) : base(car) { }
+
+    public override string GetFeatures()
+    {
+        return car.GetFeatures() + ", навигационная система";
+    }
+
+    public override double GetPrice()
+    {
+        return car.GetPrice() + 1500;
+    }
+}
+
+class LeatherSeatsDecorator : CarDecorator
+{
+    public LeatherSeatsDecorator(ICar car) : base(car) { }
+
+    public override string GetFeatures()
+    {
+        return car.GetFeatures() + ", кожаные сиденья";
+    }
+
+    public override double GetPrice()
+    {
+        return car.GetPrice() + 2000;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ICar car = new BasicCar();
+        Console.WriteLine($"{car.GetFeatures()}\nЦена: ${car.GetPrice()}\n");
+
+        ICar carWithSunroof = new SunroofDecorator(new BasicCar());
+        Console.WriteLine($"{carWithSunroof.GetFeatures()}\nЦена: ${carWithSunroof.GetPrice()}\n");
+
+        ICar fullyLoaded = new LeatherSeatsDecorator(
+                            new NavigationDecorator(
+                            new SunroofDecorator(
+                            new BasicCar())));
+        Console.WriteLine($"{fullyLoaded.GetFeatures()}\nЦена: ${fullyLoaded.GetPrice()}");
+    }
+}
---
+++ Program11.3.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+interface IUser
+{
+    string GetPermissions();
+}
+
+class AdminUser : IUser
+{
+    public string GetPermissions()
+    {
+        return "Полный доступ: чтение, запись, удаление, управление пользователями";
+    }
+}
+
+class ModeratorUser : IUser
+{
+    public string GetPermissions()
+    {
+        return "Модерация: удаление контента, блокировка пользователей";
+    }
+}
+
+class RegularUser : IUser
+{
+    public string GetPermissions()
+    {
+        return "Базовый доступ: чтение, комментирование";
+    }
+}
+
+abstract class UserFactory
+{
+    public abstract IUser CreateUser();
+}
+
+class AdminFactory : UserFactory
+{
+    public override IUser CreateUser()
+    {
+        return new AdminUser();
+    }
+}
+
+class ModeratorFactory : UserFactory
+{
+    public override IUser CreateUser()
+    {
+        return new ModeratorUser();
+    }
+}
+
+class RegularFactory : UserFactory
+{
+    public override IUser CreateUser()
+    {
+        return new RegularUser();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        UserFactory[] factories = new UserFactory[]
+        {
+            new AdminFactory(),
+            new ModeratorFactory(),
+            new RegularFactory()
+        };
+
+        foreach (UserFactory factory in factories)
+        {
+            IUser user = factory.CreateUser();
+            Console.WriteLine($"{user.GetType().Name}: {user.GetPermissions()}");
+        }
+    }
+}
